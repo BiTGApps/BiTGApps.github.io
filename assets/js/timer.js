b65c10b4fa5e9430de84ecc94c87b89f3afc9221
TheHitMan7 <krtik.vrma@gmail.com>
2024-01-07 23:19:29 +0530

    Auto close AdBlock Modal
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,3 +1,5 @@
+/* This file is part of The BiTGApps Project */
+
 var count = 9;
 var counter = setInterval(timer, 1000);
 
@@ -8,9 +10,10 @@ function timer() {
         document.getElementById("modal").disabled = false;
         document.getElementById("timed").disabled = false;
         document.getElementById("timed").innerHTML = "Okay, I'll Whitelist";
+        setTimeout(function(){$('#staticAdsModal').modal('hide')},100);
         return;
     }
     document.getElementById("modal").disabled = true;
     document.getElementById("timed").disabled = true;
-    document.getElementById("timed").innerHTML = "Please wait " + count + " seconds...";
+    document.getElementById("timed").innerHTML = "Auto close in " + count + " seconds...";
 }

a321fbe2b658b2e3955b335eca1f629747d0882e
TheHitMan7 <krtik.vrma@gmail.com>
2024-12-11 18:53:22 +0530

    Handle Release Assets

---
+++ webroot.sh
@@ -0,0 +1,60 @@
+# Android 15.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 14.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 13.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 12.1.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 12.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 11.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 10.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 9.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 8.1.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 8.0.0
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 7.1.2
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+# Android 7.1.1
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html
+awk -i inplace '{gsub("","")}1' index.html

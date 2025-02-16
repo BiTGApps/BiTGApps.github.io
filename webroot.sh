7a38436e56b17e4c185e0d84af3955dac419f5ea
TheHitMan7 <krtik.vrma@gmail.com>
2025-02-16 14:44:36 +0530

    BiTGApps v4.6

--- webroot.sh
+++ webroot.sh
@@ -1,60 +1,60 @@
 # Android 15.0.0
-awk -i inplace '{gsub("1733658688","")}1' index.html
-awk -i inplace '{gsub("101102134","")}1' index.html
-awk -i inplace '{gsub("1733657585","")}1' index.html
-awk -i inplace '{gsub("98719877","")}1' index.html
+awk -i inplace '{gsub("1733658688","1739690978")}1' index.html
+awk -i inplace '{gsub("101102134","101102400")}1' index.html
+awk -i inplace '{gsub("1733657585","1739691057")}1' index.html
+awk -i inplace '{gsub("98719877","98720142")}1' index.html
 # Android 14.0.0
-awk -i inplace '{gsub("1733658076","")}1' index.html
-awk -i inplace '{gsub("94169588","")}1' index.html
-awk -i inplace '{gsub("1733657913","")}1' index.html
-awk -i inplace '{gsub("85001515","")}1' index.html
+awk -i inplace '{gsub("1733658076","1739690668")}1' index.html
+awk -i inplace '{gsub("94169588","94169850")}1' index.html
+awk -i inplace '{gsub("1733657913","1739691568")}1' index.html
+awk -i inplace '{gsub("85001515","85001786")}1' index.html
 # Android 13.0.0
-awk -i inplace '{gsub("1733658097","")}1' index.html
-awk -i inplace '{gsub("72641076","")}1' index.html
-awk -i inplace '{gsub("1733657533","")}1' index.html
-awk -i inplace '{gsub("73399941","")}1' index.html
+awk -i inplace '{gsub("1733658097","1739690724")}1' index.html
+awk -i inplace '{gsub("72641076","72641336")}1' index.html
+awk -i inplace '{gsub("1733657533","1739691526")}1' index.html
+awk -i inplace '{gsub("73399941","73400207")}1' index.html
 # Android 12.1.0
-awk -i inplace '{gsub("1733658566","")}1' index.html
-awk -i inplace '{gsub("68741421","")}1' index.html
-awk -i inplace '{gsub("1733657959","")}1' index.html
-awk -i inplace '{gsub("69846752","")}1' index.html
+awk -i inplace '{gsub("1733658566","1739690976")}1' index.html
+awk -i inplace '{gsub("68741421","68741683")}1' index.html
+awk -i inplace '{gsub("1733657959","1739691613")}1' index.html
+awk -i inplace '{gsub("69846752","69847019")}1' index.html
 # Android 12.0.0
-awk -i inplace '{gsub("1733658402","")}1' index.html
-awk -i inplace '{gsub("70410853","")}1' index.html
-awk -i inplace '{gsub("1733657681","")}1' index.html
-awk -i inplace '{gsub("68661372","")}1' index.html
+awk -i inplace '{gsub("1733658402","1739691384")}1' index.html
+awk -i inplace '{gsub("70410853","70411117")}1' index.html
+awk -i inplace '{gsub("1733657681","1739691084")}1' index.html
+awk -i inplace '{gsub("68661372","68661633")}1' index.html
 # Android 11.0.0
-awk -i inplace '{gsub("1733658601","")}1' index.html
-awk -i inplace '{gsub("69025432","")}1' index.html
-awk -i inplace '{gsub("1733657621","")}1' index.html
-awk -i inplace '{gsub("63684989","")}1' index.html
+awk -i inplace '{gsub("1733658601","1739690575")}1' index.html
+awk -i inplace '{gsub("69025432","69025695")}1' index.html
+awk -i inplace '{gsub("1733657621","1739691024")}1' index.html
+awk -i inplace '{gsub("63684989","63685254")}1' index.html
 # Android 10.0.0
-awk -i inplace '{gsub("1733658580","")}1' index.html
-awk -i inplace '{gsub("58817614","")}1' index.html
-awk -i inplace '{gsub("1733657449","")}1' index.html
-awk -i inplace '{gsub("62586705","")}1' index.html
+awk -i inplace '{gsub("1733658580","1739691358")}1' index.html
+awk -i inplace '{gsub("58817614","58817796")}1' index.html
+awk -i inplace '{gsub("1733657449","1739690996")}1' index.html
+awk -i inplace '{gsub("62586705","62586882")}1' index.html
 # Android 9.0.0
-awk -i inplace '{gsub("1733658408","")}1' index.html
-awk -i inplace '{gsub("65569012","")}1' index.html
-awk -i inplace '{gsub("1733657557","")}1' index.html
-awk -i inplace '{gsub("63155887","")}1' index.html
+awk -i inplace '{gsub("1733658408","1739690518")}1' index.html
+awk -i inplace '{gsub("65569012","65569198")}1' index.html
+awk -i inplace '{gsub("1733657557","1739690761")}1' index.html
+awk -i inplace '{gsub("63155887","63156070")}1' index.html
 # Android 8.1.0
-awk -i inplace '{gsub("1733658088","")}1' index.html
-awk -i inplace '{gsub("63597935","")}1' index.html
-awk -i inplace '{gsub("1733658071","")}1' index.html
-awk -i inplace '{gsub("67693583","")}1' index.html
+awk -i inplace '{gsub("1733658088","1739690454")}1' index.html
+awk -i inplace '{gsub("63597935","63598120")}1' index.html
+awk -i inplace '{gsub("1733658071","1739690812")}1' index.html
+awk -i inplace '{gsub("67693583","67693762")}1' index.html
 # Android 8.0.0
-awk -i inplace '{gsub("1733658483","")}1' index.html
-awk -i inplace '{gsub("59804250","")}1' index.html
-awk -i inplace '{gsub("1733657762","")}1' index.html
-awk -i inplace '{gsub("59970848","")}1' index.html
+awk -i inplace '{gsub("1733658483","1739691306")}1' index.html
+awk -i inplace '{gsub("59804250","59804432")}1' index.html
+awk -i inplace '{gsub("1733657762","1739691151")}1' index.html
+awk -i inplace '{gsub("59970848","59971026")}1' index.html
 # Android 7.1.2
-awk -i inplace '{gsub("1733658128","")}1' index.html
-awk -i inplace '{gsub("61398635","")}1' index.html
-awk -i inplace '{gsub("1733657518","")}1' index.html
-awk -i inplace '{gsub("60207315","")}1' index.html
+awk -i inplace '{gsub("1733658128","1739690351")}1' index.html
+awk -i inplace '{gsub("61398635","61398818")}1' index.html
+awk -i inplace '{gsub("1733657518","1739691388")}1' index.html
+awk -i inplace '{gsub("60207315","60207497")}1' index.html
 # Android 7.1.1
-awk -i inplace '{gsub("1733657707","")}1' index.html
-awk -i inplace '{gsub("70226063","")}1' index.html
-awk -i inplace '{gsub("1733657937","")}1' index.html
-awk -i inplace '{gsub("58002742","")}1' index.html
+awk -i inplace '{gsub("1733657707","1739690577")}1' index.html
+awk -i inplace '{gsub("70226063","70226246")}1' index.html
+awk -i inplace '{gsub("1733657937","1739691391")}1' index.html
+awk -i inplace '{gsub("58002742","58002920")}1' index.html

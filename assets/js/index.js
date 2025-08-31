d4cba8bf11d499849708c8c055b8c4eaa2979740
TheHitMan7 <krtik.vrma@gmail.com>
2025-08-31 18:01:38 +0530

    Save for future

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,21 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function downloadSelectedFile() {
+    const selectElement = document.getElementById('MicroGPlus');
+    const selectedFile = selectElement.options[selectElement.selectedIndex].value;
+    if (selectedFile) {
+        const link = document.createElement('a');
+        link.href = selectedFile;
+        link.download = selectedFile.substring(selectedFile.lastIndexOf('/') + 1);
+        document.body.appendChild(link); // Append
+        link.click();                    // Trigger
+        document.body.removeChild(link); // Remove
+    }
+}
+document.getElementById('MicroGPlus').addEventListener('change', downloadSelectedFile);
+
+function resetSelectedFile() {
+    document.getElementById("MicroGPlus").selectedIndex = 0;
+}
